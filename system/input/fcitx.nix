dd2f015ce8d54e7abbe25ed32c3907de2052d24e
happytech <happytech.web@gmail.com>
2025-02-08 00:02:28 +0800

    add fcitx gtk

--- system/input/fcitx.nix
+++ system/input/fcitx.nix
@@ -1,12 +1,13 @@
 { pkgs, ...}: {
   i18n = {
+    # 
     defaultLocale = "en_US.UTF-8";
     inputMethod = {
       enable = true;
       type = "fcitx5";
       fcitx5 = {
         addons = with pkgs; [
-          # fcitx5-gtk
+          fcitx5-gtk
           fcitx5-rime
           fcitx5-chinese-addons
           # fcitx5-nord
