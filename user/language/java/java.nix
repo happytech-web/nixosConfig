379b64821163214a87c69c7422e4d29f08a46078
happytech <happytech.web@gmail.com>
2025-02-08 16:54:55 +0800

    add some comment

--- user/language/java/java.nix
+++ user/language/java/java.nix
@@ -2,6 +2,9 @@
 {
   home.packages = with pkgs; [
     temurin-bin
-    jetbrains.idea-community
+    jetbrains.idea-community-bin
+    # install it for emacs, but emacs just don't use it. lsp-java want to install it in emacs.d
+    # btw, you need to set export MAVEN_OPTS="-DsocksProxyHost=127.0.0.1 -DsocksProxyPort=7897" to install it succesfully in emacs
+    jdt-language-server
   ];
 }
