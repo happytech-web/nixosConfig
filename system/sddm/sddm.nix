eab8063ee1cdc9f86e34191ad98396d6135235e1
happytech <happytech.web@gmail.com>
2025-02-08 23:04:34 +0800

    change sddm

--- system/sddm/sddm.nix
+++ system/sddm/sddm.nix
@@ -4,7 +4,7 @@
     enable = true;
     package = pkgs.kdePackages.sddm;
     extraPackages = [ pkgs.sddm-astronaut ];
-    theme = "sddm-astronaut";
+    theme = "sddm-astronaut-theme";
   };
 
   environment.systemPackages = with pkgs; [
--- system/wm/wayland.nix
+++ system/wm/wayland.nix
@@ -1,5 +1,3 @@
 {pkgs, ...}:
 {
-
-  services.displayManager.sddm.enable = true;
 }
