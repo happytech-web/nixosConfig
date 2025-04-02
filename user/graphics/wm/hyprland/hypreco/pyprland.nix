9e4dc075e0f903a9b50967d0093066c6715805a0
happytech <happytech.web@gmail.com>
2025-04-02 23:12:55 +0800

    fix pyprland to make it work

--- user/graphics/wm/hyprland/hypreco/pyprland.nix
+++ user/graphics/wm/hyprland/hypreco/pyprland.nix
@@ -4,10 +4,15 @@
     pyprland
   ];
   home.file.".config/hypr/pyprland.toml".text = ''
+  [pyprland]
+  plugins = ["scratchpads"]
+
+  [scratchpads]
   [scratchpads.term]
   animation = "fromTop"
   command = "kitty --class kitty-dropterm"
   class = "kitty-dropterm"
+  position = "14% 18%"
   size = "75% 60%"
   max_size = "1920px 100%"
   margin = 50
