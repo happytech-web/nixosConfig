28252e11973b572e863fe353de14883dd7c2121d
happytech <happytech.web@gmail.com>
2025-04-12 22:10:43 +0800

    make sql support large file

--- system/apps/sql/sql.nix
+++ system/apps/sql/sql.nix
@@ -3,5 +3,16 @@
   services.mysql = {
     enable = true;
     package = pkgs.mariadb;
+    settings = {
+      mysqld = {
+        max_allowed_packet = "256M";
+        wait_timeout = 28800;         # 超时时间调整为8小时
+        interactive_timeout = 28800;
+      };
+      mysqldump = {
+        quick = true;
+        max_allowed_packet = "256M";
+      };
+    };
   };
 }
