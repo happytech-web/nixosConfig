1e40ff6b567c47b57a15a8cdbff4524672184e41
happytech <happytech.web@gmail.com>
2025-05-13 15:37:17 +0800

    add local_infile for the nju_se database lab

--- system/apps/sql/sql.nix
+++ system/apps/sql/sql.nix
@@ -4,10 +4,14 @@
     enable = true;
     package = pkgs.mariadb;
     settings = {
+      mysql = {
+        local_infile = 1;
+      };
       mysqld = {
         max_allowed_packet = "256M";
         wait_timeout = 28800;         # 超时时间调整为8小时
         interactive_timeout = 28800;
+        local_infile = 1;
       };
       mysqldump = {
         quick = true;
