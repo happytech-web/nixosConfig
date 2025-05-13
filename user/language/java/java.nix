ddf075da3ecb551eead9d96f02be60b1e808a9f1
happytech <happytech.web@gmail.com>
2025-05-13 15:36:39 +0800

    add idea ultimate

--- user/language/java/java.nix
+++ user/language/java/java.nix
@@ -3,6 +3,7 @@
   home.packages = with pkgs; [
     jdk11
     jetbrains.idea-community-bin
+    jetbrains.idea-ultimate
     # install it for emacs, but emacs just don't use it. lsp-java want to install it in emacs.d
     # btw, you need to set export MAVEN_OPTS="-DsocksProxyHost=127.0.0.1 -DsocksProxyPort=7897" to install it succesfully in emacs
     jdt-language-server
